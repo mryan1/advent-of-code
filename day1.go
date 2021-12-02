ecc8cbf2c3d418aa7d25335390e477648fad1bd2
Mark Ryan <mark.ryan@gmail.com>
2021-12-01 22:48:57 -0700

    day 1 part 2

--- day1.go
+++ day1.go
@@ -8,6 +8,14 @@ import (
 	"strconv"
 )
 
+func sum(array []int) int {
+	result := 0
+	for _, v := range array {
+		result += v
+	}
+	return result
+}
+
 func main() {
 
 	data, err := os.Open("input.txt")
@@ -17,19 +25,27 @@ func main() {
 		panic(err)
 	}
 	scanner := bufio.NewScanner(data)
-	higherCount := 0
-	var lastLine int64
-	for i := 0; scanner.Scan(); i++ {
-		currentLine, _ := strconv.ParseInt(scanner.Text(), 0, 64)
+	var readingList []int
+	for scanner.Scan() {
+		line, _ := strconv.ParseInt(scanner.Text(), 0, 64)
+		readingList = append(readingList, int(line))
 
-		if currentLine > lastLine && i != 0 {
-			higherCount = higherCount + 1
-		}
-		lastLine = currentLine
 		if err := scanner.Err(); err != nil {
 			fmt.Fprintln(os.Stderr, "reading file", err)
 		}
 	}
-	fmt.Println(higherCount)
+	//now iterate through slice, calculate sliding window and cound increases
+	var higherCounter int = 0
+	var lastWindowSum int = 0
+	var currentWindowSum int = 0
+	for t := range readingList {
+		p := t + 3
+		currentWindowSum = sum(readingList[t:p])
+		if currentWindowSum > lastWindowSum && t != 0 {
+			higherCounter = higherCounter + 1
+		}
+		lastWindowSum = currentWindowSum
+	}
+	fmt.Println(higherCounter)
 
 }
