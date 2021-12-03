b1335392f5b96f9f0147ab766454c329215874c1
Mark Ryan <mark.ryan@gmail.com>
2021-12-02 19:11:05 -0700

    files

---
+++ 2021/day2/day2_part2.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Println("Error opening file!")
+		panic(err)
+	}
+	scanner := bufio.NewScanner(data)
+	var horizontal int64 = 0
+	var depth int64 = 0
+	var aim int64 = 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		commands := strings.Split(line, " ")
+		d, _ := strconv.ParseInt(commands[1], 0, 64)
+
+		if commands[0] == "forward" {
+			horizontal = horizontal + d
+			depth = depth + (d * aim)
+		} else if commands[0] == "up" {
+			aim = aim - d
+		} else if commands[0] == "down" {
+			aim = aim + d
+		}
+
+	}
+	fmt.Println(horizontal * depth)
+}
