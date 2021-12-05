9fbe78c32afc95e28c6c2ee303ff706c73f9b6ac
Mark Ryan <mark.ryan@gmail.com>
2021-12-05 11:42:03 -0700

    need to debug

--- 2021/day4/day4_part1.go
+++ 2021/day4/day4_part1.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"reflect"
+	"strconv"
 	"strings"
 )
 
-func loadInput(fileName string, b *[][5][5]string) {
+func loadInput(fileName string, b *[][5][5]string, cn *[]string) {
 	data, err := os.Open(fileName)
 	//example of how to write to sice of arrays
 	if err != nil {
@@ -20,8 +22,7 @@ func loadInput(fileName string, b *[][5][5]string) {
 
 	// parse first row for numbers
 	scanner.Scan()
-	calledNumbers := strings.Split(scanner.Text(), ",")
-	fmt.Println(calledNumbers)
+	(*cn) = strings.Split(scanner.Text(), ",")
 
 	//parse the rest to get the boards
 	for scanner.Scan() {
@@ -43,25 +44,71 @@ func loadInput(fileName string, b *[][5][5]string) {
 			fmt.Fprintln(os.Stderr, "reading file", err)
 		}
 	}
-
-	//board := [5][5]int{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}}
-	//boards = append(boards, board)
-	//board = [5][5]int{{26, 27, 28, 29, 30}, {31, 32, 33, 34, 35}, {36, 37, 38, 39, 40}, {41, 42, 43, 44, 45}, {46, 47, 48, 49, 50}}
-	//boards = append(boards, board)
-	//fmt.Println(boards)
-
 }
 
+func boardColumn(board [5][5]string, columnIndex int) (column []string) {
+	column = make([]string, 0)
+	for _, row := range board {
+		column = append(column, row[columnIndex])
+	}
+	return
+}
+
+func updateBoards(b *[][5][5]string, cn string) int {
+	winner := []string{"*", "*", "*", "*", "*"}
+	for i, iv := range *b {
+		for j, jv := range iv {
+
+			for k, kv := range jv {
+				if kv == cn {
+					//check for winner in horizontal direction
+					if reflect.DeepEqual((*b)[i][k][0:5], winner) {
+						return i
+					} else if reflect.DeepEqual(boardColumn((*b)[i], j), winner) {
+						//check for winner in verticle direction
+						return i
+					}
+					(*b)[i][j][k] = "*"
+				}
+			}
+		}
+	}
+	return -1
+}
+func calculateWinner(wb [5][5]string, wn int) int {
+	var sum int
+	for _, iv := range wb {
+		for _, kv := range iv {
+			if kv != "*" {
+				kvi, _ := strconv.Atoi(string(kv))
+				sum = kvi + sum
+			}
+		}
+	}
+	fmt.Println("Sum: ", sum)
+	return sum
+
+}
 func main() {
 	boards := make([][5][5]string, 0)
-
+	var calledNumbers []string
 	//load numbers in slice and boards
-	loadInput("input.txt", &boards)
-	fmt.Println(boards)
-
+	loadInput("input.txt", &boards, &calledNumbers)
 	//loop though each number
+	var p int
+	//var cn int
+	for i, iv := range calledNumbers {
+		//iterate though each board and if it matches replace it with a *
+		p = updateBoards(&boards, iv)
+		if p != -1 {
+			lastCalled, _ := strconv.Atoi(string(calledNumbers[i-1]))
+			fmt.Println("Winning value: ", calculateWinner(boards[p], p)*lastCalled)
+			break
+		}
 
-	//iterate though each board and if it matches replace it with a *
+	}
+
+	//fmt.Println(boards)
 
 	//check each board as you go and see if there are 5 *'s in a row, if so we've found a winner
 
